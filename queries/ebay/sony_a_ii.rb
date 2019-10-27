19675f51ea351cbfe267c6a725ba32fefeb6832d
Dmitrii Skrylev <dima.skrylev@plansource.com>
2019-10-27 16:46:18 -0600

    added pagination

--- queries/ebay/sony_a_ii.rb
+++ queries/ebay/sony_a_ii.rb
@@ -12,8 +12,24 @@ class SonyAII < Main
 		form['_nkw'] = phrase
 		@page = form.submit
 	end
+
+	def get_all_links
+		result = []
+		@page.links.each do |link|
+			if link.dom_class == 's-item__link'
+				result << link
+			end
+		end
+		result
+	end
+
+	def pagination_count
+		last_element = "//ol[@class='x-pagination__ol']//li[@class='x-pagination__li'][last()]"
+		@page.xpath(last_element).text
+	end
 end
 
 sony_search = SonyAII.new
 sony_search.search("Sony a7 ii")
+links = sony_search.get_all_links
 p ''
