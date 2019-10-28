b1c547e8d0b2436d5abb668f64d9b2961a852b04
Dmitrii Skrylev <dima.skrylev@plansource.com>
2019-10-27 19:33:49 -0600

    Added filters

--- main.rb
+++ main.rb
@@ -2,4 +2,15 @@ require_relative "n_helper"
 
 class Main
 	include NHelper
+
+	##
+	# Saves results into JSON
+	#
+	def save_data(data)
+		container = {}
+		p ''
+		data.each do |item|
+
+		end
+	end
 end
--- queries/ebay/sony_a_ii.rb
+++ queries/ebay/sony_a_ii.rb
@@ -3,6 +3,8 @@ require '../../main'
 class SonyAII < Main
 	attr_accessor :page
 
+	PAGINATION = "//ol[@class='x-pagination__ol']//li[@class='x-pagination__li'][last()]"
+
 	def initialize
 		@page = goto("https://www.ebay.com/")
 	end
@@ -24,12 +26,43 @@ class SonyAII < Main
 	end
 
 	def pagination_count
-		last_element = "//ol[@class='x-pagination__ol']//li[@class='x-pagination__li'][last()]"
-		@page.xpath(last_element).text
+		@page.xpath(PAGINATION).text
+	end
+
+	##
+	# TODO: Something isn't working here!
+	#
+	def pagination
+		result = []
+		@page.links.each do |link|
+			if link.dom_class == "x-pagination__li"
+				result << link
+			end
+		end
+		result
+	end
+
+	#
+	# Accessors
+	#
+	def filter_ending_soonest
+		@page = self.page.link_with(xpath: "//span[.='Time: ending soonest']/..").click
+	end
+
+	def filter_best_match
+		@page = self.page.link_with(xpath: "//span[.='Best Match']/..").click
+	end
+
+	def filter_newly_listed
+		@page = self.page.link_with(xpath: "//span[.='Time: newly listed']/..").click
+	end
+
+	def filter_us_only
+		@page = self.page.link_with(xpath: "//span[.='US Only']//ancestor::a").click
 	end
 end
 
 sony_search = SonyAII.new
 sony_search.search("Sony a7 ii")
-links = sony_search.get_all_links
+sony_search.filter_newly_listed
 p ''
