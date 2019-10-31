3a157e303f5960543193f3ffd3619811d484e187
Dmitrii Skrylev <dima.skrylev@plansource.com>
2019-10-30 19:26:51 -0600

    Added rails and started Active Record setup

--- Gemfile
+++ Gemfile
@@ -6,3 +6,4 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 ruby '2.6.4'
 gem 'mechanize'
+gem "rails", "6.0"
--- queries/ebay/sony_aii_runner.rb
+++ queries/ebay/sony_aii_runner.rb
@@ -1,6 +1,13 @@
 require_relative 'ebay'
 
+def start_db
+  ActiveRecord::Base.connection.close rescue ActiveRecord::ConnectionNotEstablished
+  db_config = YAML::load(File.open("database.yml"))['default']
+  ActiveRecord::Base.establish_connection(db_config)
+end
+
 s = Ebay.new("Sony a7 ii")
 s.newly_listed = true
 s.us_only = true
 result = s.start
+p ''
